e17787c9e54c09fc99e69b8f4f3fe498220fa4ea
Pan Xiao <xiaopan@outlook.com>
2026-05-07 17:01:27 +0800

    update contract address for Sepolia network

--- frontend/app/utils/contractInfo.ts
+++ frontend/app/utils/contractInfo.ts
@@ -3,7 +3,7 @@ When switching to a different network, the contract address and owner address sh
 */
 
 // Sepolia:
-// export const CONTRACT_ADDRESS = "0xe2021e75021eF29c5438Fd463a5A37D66e71514B";
+// export const CONTRACT_ADDRESS = "0x9DF11C09eF92e6B9F30942f080490D1e413B72c4";
 // export const CONTRACT_OWNER_ADDRESS = "0xDAc57DA61035BF3811ff1FC239eCdA0Ff9931E70";
 
 // Local Hardhat:
